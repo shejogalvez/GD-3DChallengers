867aa1d7a724c5cac39389e0e53b4d41493f6b87
Juanxpeke <juanintendo.f11@gmail.com>
2022-04-07 15:24:43 -0400

    Added mechanics to the room
    Mechanics to the room in order to do a better demo, such as enemies or time count and reset.

--- Battle_Room.gd
+++ Battle_Room.gd
@@ -1,5 +1,6 @@
 extends Node
 
+var total_time = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,5 +9,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	total_time += delta
+	
 	if Input.is_key_pressed(KEY_R):
+		get_tree().reload_current_scene()
+		
+	if get_tree().get_nodes_in_group("enemies").empty():
+		print("You have won! Total time: " + str(total_time))
 		get_tree().reload_current_scene()
