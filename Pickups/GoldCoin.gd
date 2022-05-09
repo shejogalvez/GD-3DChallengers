2e826193e449dfdfa0b588b54c2b566f81e956d5
Juanxpeke <juanintendo.f11@gmail.com>
2022-05-09 00:44:51 -0400

    Basic coin pickup

---
+++ Pickups/GoldCoin.gd
@@ -0,0 +1,20 @@
+extends RigidBody
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$PickupAudio.connect("finished", self, "queue_free")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("shot_main"):
+		if (global_transform.origin - PlayerManager.get_player_position()).length() < 10:
+			$Model.queue_free()
+			if not $PickupAudio.playing:
+				$PickupAudio.play()
