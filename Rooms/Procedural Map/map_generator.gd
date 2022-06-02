a6c567a0b7e6a8db03f50713addaf8faf2a97e28
shejogalvez <71142873+shejogalvez@users.noreply.github.com>
2022-06-02 17:15:01 -0400

    added selectable room, solve bugs
    el rfc funciona cuando quiere??

--- Rooms/Procedural Map/map_generator.gd
+++ Rooms/Procedural Map/map_generator.gd
@@ -12,21 +12,27 @@ var room2pad : PackedScene = preload( "res://Rooms/Sala2x2Base2ADY.tscn" )
 var room2pad2 : PackedScene = preload( "res://Rooms/Sala2x2Base2ADY2.tscn" )
 var room2pop : PackedScene = preload("res://Rooms/Sala2x2Base2OP.tscn"  )
 var room3p : PackedScene = preload( "res://Rooms/Sala2x2Base3P.tscn" )
+var room3p2 : PackedScene = preload( "res://Rooms/Sala2x2Base3P2.tscn" )
+var room3p3 : PackedScene = preload( "res://Rooms/Sala2x2Base3P3.tscn" )
 var room4p : PackedScene = preload("res://Rooms/Sala2x2Base4P.tscn" )
 
 var leaf_rooms = Array()
 var actual_room = null
 const orientations = [Vector2(0, 1), Vector2(1, 0), Vector2(-1, 0)]
+const unique_rooms_n = 8
+const initial_weights = [5, 3, 3, 5, 3, 3, 2, 3]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rfc = RandomFunctionCaller.new()
-	rfc.put_func(0, self, "set_room", [room1p])
-	rfc.put_func(1, self, "set_room", [room2pad]) #(-1, 0)
-	rfc.put_func(1, self, "set_room", [room2pad]) #(1, 0)
-	rfc.put_func(1, self, "set_room", [room2pop]) #(0, 1)
-	rfc.put_func(1, self, "set_room", [room3p])   #(1, 0), (0, 1)
-	rfc.put_func(1, self, "set_room", [room4p])   #(-1, 0), (1, 0), (0, 1)
+	rfc.put_func(initial_weights[0], self, "set_room", [room1p])
+	rfc.put_func(initial_weights[1], self, "set_room", [room2pad]) #(-1, 0)
+	rfc.put_func(initial_weights[2], self, "set_room", [room2pad]) #(1, 0)
+	rfc.put_func(initial_weights[3], self, "set_room", [room2pop]) #(0, 1)
+	rfc.put_func(initial_weights[4], self, "set_room", [room3p])   #(1, 0), (0, 1)
+	rfc.put_func(initial_weights[5], self, "set_room", [room3p2])   #(-1, 0), (0, 1)
+	rfc.put_func(initial_weights[6], self, "set_room", [room3p3])   #(1, 0), (-1, 0)
+	rfc.put_func(initial_weights[7], self, "set_room", [room4p])   #(-1, 0), (1, 0), (0, 1)
 	var init = initial_room.instance()
 	init.initialize(0, self, Vector2.ZERO)
 	self.add_child(init)
@@ -41,7 +47,7 @@ func _ready():
 			if (len(leaf_rooms) == 1):
 				rfc.update_weight(0, 0)
 			else:
-				rfc.update_weight(0, 1)
+				rfc.update_weight(0, initial_weights[0])
 			room.generate_rooms()
 	if (len(ocuppied_spots) <= self.number_of_rooms) : print("premature exit")
 	print(ocuppied_spots)
@@ -60,24 +66,17 @@ func is_valid_room(pos:Vector2) -> bool:
 			return false
 	return true
 
-#func add_room(pos, orientation):
-#	if not is_valid_room(pos):
-#		return
-#	ocuppied_spots.append(pos)
-#
-#	var room = RandomRoom.new()
-#	room.initialize(orientation, orientations, self, pos)
-#	leaf_rooms.append()
-
 func construct_room(pos, angle):
 	ocuppied_spots.append(pos)
 	set_avaliable_rooms(pos, angle)
-	
-	rfc.call_func()
+	print(rfc.weights)
+	var prev = actual_room
+	while (prev == actual_room):
+		rfc.call_func()
 	leaf_rooms.append(actual_room)
-	return_pool_values()
-	print(actual_room)
-	#print("room ", pos, "= ", actual_room)
+	reset_pool_values()
+	print("room ", pos, "= ", actual_room)
+	print(ocuppied_spots)
 	return actual_room
 
 func room_done(room):
@@ -90,37 +89,45 @@ func room_done(room):
 func set_avaliable_rooms(pos, angle):
 	var results = [false, false, false]
 	for i in range(3):
-		var dir = orientations[i].rotated(angle) + pos
+		var dir = orientations[i].rotated(-angle)
+		print(pos + dir)
 		for spot in ocuppied_spots:
 			if (spot - (pos + dir)).length() < 0.1:
+				print(spot, " already taken ", i)
 				results[i] = true
 				break
 	update_room_pool(results)
 	
 	
 # orientations = [(0, 1), (1, 0), (-1, 0)]
-#	rfc.put_func(0, self, "set_room", [room1p])
+#	rfc.put_func(0, self, "set_room", [room1p]) 0
 #	rfc.put_func(1, self, "set_room", [room2pad]) #(-1, 0)  1
 #	rfc.put_func(1, self, "set_room", [room2pad]) #(1, 0)  2 
 #	rfc.put_func(1, self, "set_room", [room2pop]) #(0, 1)  3
 #	rfc.put_func(1, self, "set_room", [room3p])   #(1, 0), (0, 1)  4
-#	rfc.put_func(1, self, "set_room", [room4p])   #(-1, 0), (1, 0), (0, 1)  5
+#	rfc.put_func(1, self, "set_room", [room3p2])   #(-1, 0), (0, 1)  5
+#	rfc.put_func(1, self, "set_room", [room3p3])   #(1, 0), (-1, 0)  6
+#	rfc.put_func(1, self, "set_room", [room4p])   #(-1, 0), (1, 0), (0, 1)  7
 func update_room_pool(bool_array):
 	if bool_array[0]:
 		rfc.update_weight(3, 0)
 		rfc.update_weight(4, 0)
 		rfc.update_weight(5, 0)
+		rfc.update_weight(7, 0)
 	if bool_array[1]:
 		rfc.update_weight(2, 0)
 		rfc.update_weight(4, 0)
-		rfc.update_weight(5, 0)
+		rfc.update_weight(6, 0)
+		rfc.update_weight(7, 0)
 	if bool_array[2]:
 		rfc.update_weight(1, 0)
 		rfc.update_weight(5, 0)
+		rfc.update_weight(6, 0)
+		rfc.update_weight(7, 0)
 		
-func return_pool_values():
-	for i in range(6):
-		rfc.update_weight(i, 1)
+func reset_pool_values():
+	for i in range(unique_rooms_n):
+		rfc.update_weight(i, initial_weights[i])
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
--- Rooms/Procedural Map/random_room.gd
+++ Rooms/Procedural Map/random_room.gd
@@ -6,8 +6,11 @@ var father
 var pos = Vector2()
 export (Array, Vector2) var openings
 const size = 64.1
+var global_size = 64.1
 const separation = 300
 const offset = 14
+const size_variation = 2
+var scale_factor = 1
 
 export (PackedScene) var teleport : PackedScene = preload("res://Rooms/Procedural Map/TeleportENbase2.tscn")
 export (PackedScene) var wall : PackedScene = preload("res://Rooms/WALL.tscn")
@@ -20,6 +23,11 @@ func initialize(angle: float, father, pos: Vector2):
 	self.father = father
 	self.angle = angle
 	self.pos = pos
+	#scale_factor = rand_range(1, size_variation)
+	#self.size = scale_factor * base_size
+	#self.scale = Vector3((self.scale.x * scale_factor)  / prev_scale, 1, (self.scale.z * scale_factor)  / prev_scale)
+	#print(self.scale, " ", prev_scale)
+	#global_size = size * scale_factor
 	# se añade un tp de vuelta al crearse la sala
 	var back_tp = teleport.instance()
 	back_tp.translation = Vector3.FORWARD * size
@@ -36,11 +44,11 @@ func generate_rooms():
 		var newpos = self.pos + new_opening
 		var newangle = -Vector2.DOWN.angle_to(new_opening)
 		var relangle = newangle - self.angle
-		#print(self.pos, " creando sala en posicion " , newpos, ", angulo absoluto %f y relativo %f"  % [newangle, relangle])
+		#print(self,self.pos, " creando sala en posicion " , newpos, ", angulo absoluto %f y relativo %f"  % [newangle, relangle])
 		if father.is_valid_room(newpos):
 			var new_room : RandomRoom = father.construct_room(newpos, newangle)
-			#new_room.global_transform = self.global_transform
-			new_room.translation = self.separation * Vector3(rel_opening.x, 0, rel_opening.y)
+			
+			new_room.translation = self.separation/scale_factor * Vector3(rel_opening.x, 0, rel_opening.y)
 			new_room.rotate_y(relangle)
 			new_room.initialize(newangle, self.father, newpos)
 			self.add_child(new_room)
@@ -48,7 +56,7 @@ func generate_rooms():
 			var for_tp = teleport.instance()
 			for_tp.translation = Vector3(rel_opening.x, 0, rel_opening.y) * size
 			for_tp.rotate_y(relangle)
-			for_tp.set_distance(Vector3(new_opening.x, 0, new_opening.y), separation - 2*size, offset) 
+			for_tp.set_distance(Vector3(new_opening.x, 0, new_opening.y), separation - 2*global_size, offset) 
 			self.add_child(for_tp)
 		else:
 			var index = openings.find(opening)
