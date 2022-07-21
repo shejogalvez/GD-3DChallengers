56dd8d194808cec711e1adda13e1f3b05a038707
Karnow11 <andresignaciobc@gmail.com>
2022-07-20 20:15:24 -0400

    Salita Inicio Que pro

--- Items/Item.gd
+++ Items/Item.gd
@@ -4,11 +4,12 @@ class_name Item, "res://Assets/Classes/item_icon.png"
 # Item image
 export(StreamTexture) var item_image
 # Item name
-export(String) var item_name = "Item"
+export(String) var item_name = "Welcome"
 # Item description (lore)
-export(String) var item_description = "This is an standard item intended to be extended"
+export(String) var item_description = "Controls:           Movement: WASD     Shoot: Left Click          Alt Shoot: Right Click           Sprint: Shift              Jump: Space Bar               Good Luck"
+
 # Item effect (gameplay)
-export(String) var item_effect = "This item does nothing"
+export(String) var item_effect = ""
 
 # Called when the player uses the item.
 func use() -> void:
---
+++ PedestalTutorial.gd
@@ -0,0 +1,87 @@
+extends Spatial
+export(PackedScene) var Mostrador = preload("res://Items/ItemTutorial.tscn")
+
+var rng := RandomNumberGenerator.new()
+
+var items_pool := []
+
+var item_instance : Item
+
+const SPRITE_WIDTH := 640
+const SPRITE_HEIGHT := 640
+
+onready var item = $Item
+onready var item_model = $Item/ItemModel
+onready var item_sprite_viewport = $Item/ItemModel/SpriteViewport
+onready var item_sprite = $Item/ItemModel/SpriteViewport/Sprite
+onready var item_pickup_area = $Item/PickupArea
+onready var item_animation_player= $Item/ItemAnimationPlayer
+onready var item_details_area = $Item/DetailsArea
+onready var item_details_model = $Item/DetailsModel
+onready var item_details_viewport = $Item/DetailsModel/DetailsViewport
+onready var item_details_sprite = $Item/DetailsModel/DetailsViewport/DetailsPanel/DetailsSprite
+onready var item_details_name = $Item/DetailsModel/DetailsViewport/DetailsPanel/DetailsHeader/DetailsName
+onready var item_details_description = $Item/DetailsModel/DetailsViewport/DetailsPanel/DetailsHeader/DetailsDescription
+onready var item_details_effect = $Item/DetailsModel/DetailsViewport/DetailsPanel/DetailsEffect
+onready var item_details_animation_player = $Item/DetailsAnimationPlayer
+onready var item_pedestal_audio = $ItemPedestalAudio
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	rng.randomize()
+	items_pool = [Mostrador]
+	item_instance = items_pool[rng.randi_range(0, items_pool.size() - 1)].instance()
+	_initialize_item_model(item_instance.item_image)
+	#item_pickup_area.connect("body_entered", self, "_use_item")
+	item_details_area.connect("body_entered", self, "_show_details")
+	item_details_area.connect("body_exited", self, "_hide_details")
+	_initialize_details_model()
+	item_details_sprite.texture = item_instance.item_image
+	item_details_name.text = item_instance.item_name
+	item_details_description.text = item_instance.item_description
+	item_details_effect.text = item_instance.item_effect
+	item_details_model.hide()
+	item.add_child(item_instance)
+
+# Gives the Item if area touched by a player.
+func _use_item(_body) -> void:
+	item_details_animation_player.play("RESET")
+	item_instance.use()
+	item_pedestal_audio.play()
+	item.queue_free()
+	
+# Shows the item details.
+func _show_details(_body) -> void:
+	item_details_animation_player.play("details_appear")
+	
+# Hides the item details.
+func _hide_details(_body) -> void:
+	item_details_animation_player.play("details_disappear")
+
+# Initializes the item model.
+func _initialize_item_model(image : StreamTexture) -> void:
+	var material := SpatialMaterial.new()
+	# Always looks towards the camera
+	material.params_billboard_mode = SpatialMaterial.BILLBOARD_ENABLED
+	# Enables transparent color rendering
+	material.flags_transparent = true
+	# Sets the texture to be the image 
+	item_sprite.texture = image
+	var width_factor = SPRITE_WIDTH / image.get_width()
+	var height_factor = SPRITE_HEIGHT / image.get_height()
+	item_model.mesh = QuadMesh.new()
+	item_model.mesh.set_size(Vector2(width_factor, height_factor))
+	material.albedo_texture = item_sprite_viewport.get_texture()
+	item_model.set_surface_material(0, material)
+
+# Initializes the details model.
+func _initialize_details_model() -> void:
+	var material := SpatialMaterial.new()
+	# Render in both sides
+	material.params_cull_mode = SpatialMaterial.CULL_DISABLED
+	# Enable transparent color rendering
+	material.flags_transparent = true
+	item_details_model.mesh = QuadMesh.new()
+	item_details_model.mesh.set_size(Vector2(9, 6))
+	material.albedo_texture = item_details_viewport.get_texture()
+	item_details_model.set_surface_material(0, material)
