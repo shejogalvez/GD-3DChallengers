76a043619cf376de91a0c7cab6525cf870026e6f
Juanxpeke <juanintendo.f11@gmail.com>
2022-07-24 18:04:09 -0400

    Deleted more trash

--- Rooms/Procedural Map/random_room.gd
+++ Rooms/Procedural Map/random_room.gd
@@ -19,7 +19,7 @@ var unused_openings = Array()
 var used_openings := Array() # save pairs dir, angle in local space
 var locks_array := Array()
 
-export (PackedScene) var teleport : PackedScene = preload("res://Rooms/Test Rooms Stuff/TeleportENbase2.tscn")
+export (PackedScene) var teleport : PackedScene = preload("res://Rooms/Procedural Map/TeleportENbase2.tscn")
 export (PackedScene) var wall : PackedScene = preload("res://Assets/Meshes/AztecMesh/ParedRelleno.tscn")
 export (PackedScene) var lock_scene : PackedScene = preload("res://Rooms/Procedural Map/door_lock.tscn")
 var filled : bool = false
--- Tests/SmallTarget.gd
+++
@@ -1,7 +0,0 @@
-extends StaticBody
-
-var hit_counter = 0
-
-func projectile_hit(damage, transform):
-	hit_counter += 1
-	print("Hit number " + str(hit_counter) + "!!!")
