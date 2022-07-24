dc8f32432114a43997b0b2bdca3f9f649052d4b3
Juanxpeke <juanintendo.f11@gmail.com>
2022-07-24 18:54:42 -0400

    Even more refactoring

--- Rooms/Rooms/BigBossRoom.gd
+++ Rooms/Rooms/BigBossRoom.gd
@@ -1,9 +1,9 @@
 extends RandomRoom
 
 onready var area = $Area
-onready var env = $WorldEnvironment
 export (PackedScene) var bossScene = preload("res://enemies/attack_patterns/Remiel.tscn")
 export (Vector3) var boss_initial_pos = Vector3(0, 7, 60)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	area.connect("body_entered", self, "start_fight")
@@ -18,7 +18,3 @@ func start_fight(body):
 		boss.phase2_position = Vector3(0, 30, 0)
 		boss.ground_height = 0.5
 		area.queue_free()
-		env.environment = load("res://Rooms/Rooms2.0Andrew/BigBossRoom_env.tres")
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
