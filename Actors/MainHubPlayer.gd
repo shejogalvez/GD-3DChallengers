696361154d371c21dfaab2947edb2d6e6974247e
Juanxpeke <juanintendo.f11@gmail.com>
2022-08-08 23:50:10 -0400

    Best Player Controller Ever Made

--- Actors/MainHubPlayer.gd
+++ Actors/MainHubPlayer.gd
@@ -17,7 +17,13 @@ const MAX_SPRINT_SPEED := 48
 const SPRINT_ACCEL := 16
 var is_sprinting := false
 # Crouching
-const MAX_CROUCH_SPEED := 12
+const MAX_CROUCH_SPEED := 14
+const CROUCH_ACCEL := 1.0
+const CROUCH_IMPULSE_MULTIPLIER := 1.6
+const CROUCH_IMPULSE_MARGIN := 5.0
+const CROUCH_LERP := 30
+var default_hitbox_height := 10
+var crouch_hitbox_height := 3
 var is_crouching := false
 # Movement vectors
 var topdown_direction := Vector2()
@@ -32,20 +38,22 @@ onready var hitbox := $Hitbox
 onready var head_pivot := $HeadPivot
 onready var head_camera := $HeadPivot/Camera
 onready var head_camera_animation_player := $HeadPivot/Camera/AnimationPlayer
-onready var crouch_position := $CrouchPosition
 
 # DEBUGGING
 var jump_init := Vector3()
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
 	
 # Called each frame.
 func _physics_process(delta : float) -> void:
 	_process_input(delta)
 	_process_movement(delta)
 	_process_animations(delta)
+
 
 # Will be where we store all the code relating to player input. We want
 # to call it first, before anything else, so we have fresh player input
@@ -60,20 +68,39 @@ func _process_input(_delta : float) -> void:
 		print("Distance jumped: " + str(distance))
 	if Input.is_action_pressed("movement_jump") and is_on_floor():
 		velocity.y = JUMP_SPEED
-		jump_init = global_transform.origin	
+		jump_init = global_transform.origin
 		is_jumping = true
 		
 	# Sprinting
-	if Input.is_action_pressed("movement_sprint") and is_on_floor() and direction:
+	if Input.is_action_pressed("movement_sprint") and is_on_floor() and !is_crouching and direction:
 		is_sprinting = true
 	else:
 		is_sprinting = false
 		
 	# Crouching
-	if Input.is_action_pressed("movement_crouch") and is_on_floor():
-		pass
-	else:
-		pass
+	if Input.is_action_pressed("movement_crouch"):
+		var horizontal_velocity := velocity
+		horizontal_velocity.y = 0
+		
+		if horizontal_velocity.length() >= MAX_SPRINT_SPEED - CROUCH_IMPULSE_MARGIN and !is_crouching:
+			velocity.x = velocity.normalized().x * MAX_SPRINT_SPEED * CROUCH_IMPULSE_MULTIPLIER  
+			velocity.z = velocity.normalized().z * MAX_SPRINT_SPEED * CROUCH_IMPULSE_MULTIPLIER
+		
+		hitbox.shape.height -= CROUCH_LERP * _delta
+		hitbox.translation.y -= CROUCH_LERP * _delta * 0.5
+		head_pivot.translation.y -= CROUCH_LERP * _delta
+		is_crouching = true
+	elif !_is_on_ceiling():
+		hitbox.shape.height += CROUCH_LERP * _delta
+		hitbox.translation.y += CROUCH_LERP * _delta * 0.5
+		head_pivot.translation.y += CROUCH_LERP * _delta
+		if hitbox.shape.height >= default_hitbox_height:
+			is_crouching = false
+
+	hitbox.shape.height = clamp(hitbox.shape.height, crouch_hitbox_height, default_hitbox_height)
+	hitbox.translation.y = clamp(hitbox.translation.y, hitbox.shape.radius + crouch_hitbox_height * 0.5, hitbox.shape.radius + default_hitbox_height * 0.5)
+	head_pivot.translation.y = clamp(head_pivot.translation.y, hitbox.shape.radius + crouch_hitbox_height + 1.0, hitbox.shape.radius + default_hitbox_height + 1.0)
+	
 	
 # Process input if current view is on head camera.
 func _process_head_input(_delta : float) -> void:
@@ -103,10 +130,11 @@ func _process_head_input(_delta : float) -> void:
 	direction = direction.normalized()
 	
 	# ADS
-	if Input.is_action_pressed("aim_down_sights"):		
+	if Input.is_action_pressed("aim_down_sights"):
 		head_camera.fov = lerp(head_camera.fov, ads_fov, ADS_LERP * _delta)
 	else:
 		head_camera.fov = lerp(head_camera.fov, default_fov, ADS_LERP * _delta)
+		
 		
 # Process player's movement.
 func _process_movement(delta : float) -> void:
@@ -119,6 +147,8 @@ func _process_movement(delta : float) -> void:
 	var horizontal_velocity_target := direction
 	if is_sprinting:
 		horizontal_velocity_target *= MAX_SPRINT_SPEED
+	elif is_crouching:
+		horizontal_velocity_target *= MAX_CROUCH_SPEED
 	elif is_on_floor():
 		horizontal_velocity_target *= MAX_SPEED
 	else:
@@ -127,12 +157,14 @@ func _process_movement(delta : float) -> void:
 	
 	# Acceleration magnitude
 	var acceleration : float
-	if direction.dot(horizontal_velocity) > 0:
+	if is_crouching and horizontal_velocity.length() >= MAX_SPRINT_SPEED - CROUCH_IMPULSE_MARGIN:
+		acceleration = CROUCH_ACCEL # Smallest
+	elif direction.dot(horizontal_velocity) > 0:
 		# The two vectors points towards the same direction.
 		if is_sprinting:
 			acceleration = SPRINT_ACCEL # Biggest
 		else:
-			acceleration = ACCEL # Smallest
+			acceleration = ACCEL # Smaller
 	else:
 		# One is 0 or has a component in the opposite direction of the other.
 		# The change of velocity needs to be fast, otherwise "efecto patin".
@@ -163,7 +195,6 @@ func _process_animations(_delta : float) -> void:
 	if is_sprinting:
 		head_camera_animation_player.play("head_bob")
 	else:
-		#$Model/Casual_Hoodie/AnimationPlayer.play("Idle")
 		head_camera_animation_player.stop()
 
 
@@ -178,4 +209,9 @@ func _input(event):
 		head_pivot.rotate_x(deg2rad(cursor_position.y * MOUSE_SENSITIVITY))
 		# Limit the vertical rotation angle.
 		head_pivot.rotation.x = clamp(head_pivot.rotation.x, deg2rad(-MAX_VISION_ANGLE), deg2rad(MAX_VISION_ANGLE))
+
+
+# Returns true if is on ceiling. False otherwise.
+func _is_on_ceiling() -> bool:
+	return test_move(global_transform, Vector3(0, 2.0, 0))
 
